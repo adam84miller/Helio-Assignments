704eabefa70826cb037aed341b0b021fd8772d4a
adam84miller <adam.84.miller@gmail.com>
2020-08-20 13:30:57 -0600

    added frontendEJS

---
+++ frontendEJS/routes/food.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const fs = require("fs");
+const path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+    let foods = {}
+
+try {
+
+  const data = fs.readFileSync(path.join(__dirname, '../bin/data/food.JSON'), 'utf8')
+  foods = JSON.parse(data)
+
+} catch (err) {
+
+  console.error(err)
+}
+
+  res.render('food', foods);
+});
+
+module.exports = router;
